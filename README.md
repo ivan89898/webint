f6ce558e1775718b1d3cb9a0b937240000a45aec
ivan89898 <chizhevskiy.ivan@mail.ru>
2022-08-15 01:16:31 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/f81tbcixcktu2bdq?svg=true)](https://ci.appveyor.com/project/ivan89898/webint)
---
+++ src/test/java/AppOrderTest.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class AppOrderTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void SetUpAll() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\driver\\chromedriver.exe");
+    }
+
+    @BeforeEach
+    void SetUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void TearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void ShouldTestV1() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        List<WebElement> inputs = driver.findElements(By.tagName("input"));
+        inputs.get(0).sendKeys("Вася");
+        inputs.get(1).sendKeys("+75555555555");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        Assertions.assertEquals(expected, actual);
+    }
+}
